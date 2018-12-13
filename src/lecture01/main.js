1f88edf52d1641cc8e360e10bed16ad4121a0119
dajeong128 <dajeong128@naver.com>
2018-12-13 14:20:29 +0900

    add fucntion search

--- src/lecture01/main.js
+++ src/lecture01/main.js
@@ -5,22 +5,27 @@ window.addEventListener('scroll', function(event) {
     imgs.forEach(function(img) {
         var slideInAt = window.scrollY + window.innerHeight - img.height / 2;
         var isHalfShown = slideInAt > img.offsetTop + siteWrap.offsetTop;
-
-        if (isHalfShown) {
+        const imgBottom = siteWrap.offsetTop + img.offsetTop + img.height;
+        const isNotPassed = window.scrollY < imgBottom;
+        if (isHalfShown && isNotPassed) {
             img.classList.add('slide-in');
+        } else {
+            img.classList.remove('slide-in');
         }
     })
 })
-
 
 window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
     function myFunction() {
-    if (window.pageYOffset >= sticky ) {
-      navbar.classList.add("fixed-navbar")
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("fixed-navbar");
+        document.body.style.paddingTop = navbar.offsetHeight + 'px';
     } else {
-      navbar.classList.remove("fixed-navbar");
+        console.log('removed..')
+         navbar.classList.remove("fixed-navbar");
+         document.body.style.paddingTop = 0;
     }
   }
