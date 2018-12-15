f34509558c923b47bec69ee184f9ee6eac58e736
dajeong128 <dajeong128@naver.com>
2018-12-15 10:12:17 +0900

    add onClick

--- src/lecture02/main.js
+++ src/lecture02/main.js
@@ -12,11 +12,12 @@
 
 let data;
 const req = new XMLHttpRequest();
-req.open ('GET', 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=b0cac97aa508433ca9835e54ab51d7cd&peopleNm=x', true);
+req.open ('GET', 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=b0cac97aa508433ca9835e54ab51d7cd&peopleNm', true);
 req.onreadystatechange = function (aEvt) {
     if (req.readyState ===4) {
         if (req.status ===200) {
             data = JSON.parse(req.responseText);
+
             const person = data.peopleListResult.peopleList[0];
             console.log(person);
             const name = person.peopleNm;
@@ -38,19 +39,18 @@ req.onreadystatechange = function (aEvt) {
 }
 req.send();
 
-function enterSearch() {
-    if (event.keyCode == 13 ) {
-        myFunction();
-    } else if (event.type =='click') {
-        myFunction();   
-    }
-}
 
-function myFunction() {
-    const x = document.getElementById(search).value; {
-        if (x === name) {
-            window.location.href = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=b0cac97aa508433ca9835e54ab51d7cd&peopleNm="+"x";
-        }
-    } 
+
+document.getElementById('search').addEventListener("click", onClick); {
+    
+}function onClick(){
+    let searchValue = document.getElementById("searchBar").value;
+    for(searchValue in name) {
+        let search = JSON.stringify(searchValue);
+        // let oSearch = JSON.parse(search);
+        let name = search
+        let nameEl = document.querySelector('[data-name] h1');
+        nameEl.innerHTML = name;
+    }
 }
 
