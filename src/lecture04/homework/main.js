a710600dcd6f4585f41a3c4f604fe9142a46c00b
dajeong128 <dajeong128@naver.com>
2019-01-05 16:56:49 +0900

    make a header

--- src/lecture04/homework/main.js
+++ src/lecture04/homework/main.js
@@ -1,20 +1,16 @@
 setInterval(function(){
     var date = new Date();
-    updateTime(date);
     updateClock(date);
   }, 1000);
   
   function updateClock(date){
-    var secHand = document.querySelector(".sec-hand").style;
-    var minHand = document.querySelector(".min-hand").style;
-    var hrHand = document.querySelector(".hr-hand").style;
+    var secHand = document.querySelector("[data-second]").style;
+    var minHand = document.querySelector("[data-minute]").style;
+    var hrHand = document.querySelector("[data-hour]").style;
     
     secHand.transform = "rotate(" + date.getSeconds() * 6 + "deg)";
+    secHand.transitionTimingFunction = "cubic-bezier(0.1, 2.7, 0.58, 1)";
     minHand.transform = "rotate(" + date.getMinutes() * 6 + "deg)";
     hrHand.transform = "rotate(" + (date.getHours() * 30 + date.getMinutes() * 0.5) + "deg)";
   }
   
-  function updateTime(date){
-    var timeDiv = document.querySelector("#time");
-    timeDiv.innerHTML.hidden = true;
-  }
---
+++ src/lecture05/main.js
@@ -0,0 +1,56 @@
+const keepInCart = e => {
+    Toastify({
+        text: "장바구니에 추가되었습니다.",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        positionLeft: false, // `true` or `false`
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+      }).showToast();
+}
+
+const instance = axios.create({
+    baseURL: 'https://goods-204a7.firebaseio.com/'
+})
+
+const data = {
+    'name': '123',
+    'price': 210000
+}
+// instance.post('goods.json', data)
+instance.get('goods.json')
+    .then(res => {
+        const data = res.data;
+        const contents = document.querySelector('goods-contents');
+        const ul = document.querySelector('.goods-items')
+        const items = [];
+
+        for(let key in data) {
+            let options = [];
+            let optionList = data[key].options
+            for(let id in optionList) {
+                options.push( `
+                <option>
+                    ${optionList[id].color} ${optionList[id].size} ${optionList[id].stock}
+                </option>
+                `)
+            }
+            items.push(`
+            <li>
+                <img src="https://picsum.photos/200/300/?random">
+                <p class="goods-name">${data[key].name}</p>
+                <select>
+                ${options.join('')};
+                </select>
+                <p class="goods-seller">${data[key].provider}</p>
+                <p class="goods-price">${data[key].price.toLocaleString()}원</p>
+                <span class="goods-shippingPrice">+${data[key].shipping.price.toLocaleString()}원</span>
+                <i class="fas fa-cart-plus" style="float:right; cursor:pointer;" onclick="keepInCart(event)"></i>
+             </li>
+            `)
+        }
+        ul.innerHTML = items.join('');
+
+    });
