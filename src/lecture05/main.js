f41cde5a87feb71039a8a651ef2e2d1a93f61232
uoon-dev <uoon.dev@gmail.com>
2019-01-12 11:29:52 +0900

    refactor lecture05

--- src/lecture05/main.js
+++ src/lecture05/main.js
@@ -1,32 +1,36 @@
+// const goods = [];
+
 const instance = axios.create({
-    baseURL: 'https://goods-204a7.firebaseio.com/'
+  baseURL: 'https://goods-204a7.firebaseio.com/'
 })
 
 
 const data = {
-    'name': '123',
-    'price': 210000
+  'name': '123',
+  'price': 210000
 }
 // instance.post('goods.json', data)
 instance.get('goods.json')
-    .then(res => {
-        const data = res.data;
-        const contents = document.querySelector('goods-contents');
-        const ul = document.querySelector('.goods-items')
-        const items = [];
+  .then(res => {
+    const data = res.data;
+    const contents = document.querySelector('goods-contents');
+    const ul = document.querySelector('.goods-items')
+    const items = [];
 
+    for (let key in data) {
+      let options = [];
+      let optionList = data[key].options
 
-        for (let key in data) {
-            let options = [];
-            let optionList = data[key].options
-            for (let id in optionList) {
-                options.push(`
-                <option>
+      // goods.push(data[key]);
+
+      for (let id in optionList) {
+        options.push(`
+                <option value="${Object.keys(data).indexOf(key)}">
                     ${optionList[id].color} ${optionList[id].size} ${optionList[id].stock}
                 </option>
                 `)
-            }
-            items.push(`
+      }
+      items.push(`
             <li>
                 <img src="https://picsum.photos/200/300/?random">
                 <p class="goods-name">${data[key].name}</p>
@@ -35,38 +39,54 @@ instance.get('goods.json')
                 </select>
                 <p class="goods-seller">${data[key].provider}</p>
 
-                <i class="fas fa-cart-plus" style="float:right; cursor:pointer;" onclick="keepInCart(event)"></i>
+                <i class="fas fa-cart-plus" style="float:right; cursor:pointer;" onclick="keepInCart(event, ${Object.keys(data).indexOf(key)})"></i>
              </li>
             `)
-        }
-        ul.innerHTML = items.join('');
+    }
+    ul.innerHTML = items.join('');
 
-        const dataKey = data[key];
-        const goodsValue = [
-            dataKey.name,
-            dataKey.provider,
-            dataKey.price,
-            optionList[id],
-            optionListId.color,
-            optionListId.size,
-            optionListId.stock,
-            dataKey.shipping.price
-            ]
-    });
+    // const dataKey = data[key];
+    // const goodsValue = [
+    //     dataKey.name,
+    //     dataKey.provider,
+    //     dataKey.price,
+    //     optionList[id],
+    //     optionListId.color,
+    //     optionListId.size,
+    //     optionListId.stock,
+    //     dataKey.shipping.price
+    //     ]
+  });
 
 function openCartList() {
-    window.open("http://localhost:3006/src/lecture05/shoppingCart/shoppingCart.html");
+  window.open("http://localhost:3006/src/lecture05/shoppingCart/shoppingCart.html");
 }
 
 
-const keepInCart = e => {
+const keepInCart = (e, id) => {
+  console.log(id);
+  // 1. by client 
+  // const item = goods.filter(value => value.id === id);
+  // // instance.post('cart.json', item[0])
+  // //   .then(res => console.log(res))
+  // //   .catch(err => console.error(err));
 
-    axios({
-        method: 'post',
-        url: 'https://goods-204a7.firebaseio.com/goods/getShoppingData',
-        data: {goodsValue},
-        success: function () {
-            Toastify({
+  // 2. by connect with server
+  instance.get(`goods/${id}.json`)
+    .then(res => {
+      // 1. 
+      // res.data.options = e.target.parentElement.querySelector('select').value;
+
+      // 2.
+      const targetValue = e.target.parentElement.querySelector('select').selectedOptions[0].value;
+      instance.get(`goods/${id}/options/${targetValue}.json`)
+        .then((optionData) => {
+          debugger;
+          res.data.options = optionData.data;
+
+          instance.post('cart.json', res.data)
+            .then(res => {
+              Toastify({
                 text: "장바구니에 추가되었습니다.",
                 duration: 3000,
                 destination: "https://github.com/apvarun/toastify-js",
@@ -75,26 +95,48 @@ const keepInCart = e => {
                 gravity: "top", // `top` or `bottom`
                 positionLeft: false, // `true` or `false`
                 backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
-            }).showToast();
-        },
-        error: function () {
-            Toastify({
-                text: "실패하였습니다.",
-                duration: 3000,
-                destination: "https://github.com/apvarun/toastify-js",
-                newWindow: true,
-                close: true,
-                gravity: "top", // `top` or `bottom`
-                positionLeft: false, // `true` or `false`
-                backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
-            }).showToast();
-
-        }
+              }).showToast();
+            })
+            .catch(err => console.error(err));
+        })
     })
+    .catch(err => console.error(err));
+
+  //   )
+  //   .catch(err => console.error(err));
+  // axios({
+  //     method: 'post',
+  //     url: 'https://goods-204a7.firebaseio.com/goods/getShoppingData',
+  //     data: {goodsValue},
+  //     success: function () {
+  //         Toastify({
+  //             text: "장바구니에 추가되었습니다.",
+  //             duration: 3000,
+  //             destination: "https://github.com/apvarun/toastify-js",
+  //             newWindow: true,
+  //             close: true,
+  //             gravity: "top", // `top` or `bottom`
+  //             positionLeft: false, // `true` or `false`
+  //             backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+  //         }).showToast();
+  //     },
+  //     error: function () {
+  //         Toastify({
+  //             text: "실패하였습니다.",
+  //             duration: 3000,
+  //             destination: "https://github.com/apvarun/toastify-js",
+  //             newWindow: true,
+  //             close: true,
+  //             gravity: "top", // `top` or `bottom`
+  //             positionLeft: false, // `true` or `false`
+  //             backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+  //         }).showToast();
+  //     }
+  // })
 
 };
-           
-    
+
+
 
 
 
