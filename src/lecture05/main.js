2d674c68ac913785552fb39ebe5310d7ebe90d7e
dajeong128 <dajeong128@naver.com>
2019-01-12 10:14:32 +0900

    change to  a elements from onclick about rink and change font color in nav bar

--- src/lecture05/main.js
+++ src/lecture05/main.js
@@ -1,19 +1,7 @@
-const keepInCart = e => {
-    Toastify({
-        text: "장바구니에 추가되었습니다.",
-        duration: 3000,
-        destination: "https://github.com/apvarun/toastify-js",
-        newWindow: true,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        positionLeft: false, // `true` or `false`
-        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
-      }).showToast();
-}
-
-const instance = axios.create({ 
+const instance = axios.create({
     baseURL: 'https://goods-204a7.firebaseio.com/'
 })
+
 
 const data = {
     'name': '123',
@@ -27,11 +15,12 @@ instance.get('goods.json')
         const ul = document.querySelector('.goods-items')
         const items = [];
 
-        for(let key in data) {
+
+        for (let key in data) {
             let options = [];
             let optionList = data[key].options
-            for(let id in optionList) {
-                options.push( `
+            for (let id in optionList) {
+                options.push(`
                 <option>
                     ${optionList[id].color} ${optionList[id].size} ${optionList[id].stock}
                 </option>
@@ -45,16 +34,67 @@ instance.get('goods.json')
                 ${options.join('')};
                 </select>
                 <p class="goods-seller">${data[key].provider}</p>
-                <p class="goods-price">${data[key].price.toLocaleString()}원</p>
-                <span class="goods-shippingPrice"> +배송료 ${data[key].shipping.price.toLocaleString()}원</span>
+
                 <i class="fas fa-cart-plus" style="float:right; cursor:pointer;" onclick="keepInCart(event)"></i>
              </li>
             `)
         }
         ul.innerHTML = items.join('');
 
+        const dataKey = data[key];
+        const goodsValue = [
+            dataKey.name,
+            dataKey.provider,
+            dataKey.price,
+            optionList[id],
+            optionListId.color,
+            optionListId.size,
+            optionListId.stock,
+            dataKey.shipping.price
+            ]
     });
 
-    function openCartList() {
-        window.open("http://localhost:3006/src/lecture05/shoppingCart/shoppingCart.html");
-      }
+function openCartList() {
+    window.open("http://localhost:3006/src/lecture05/shoppingCart/shoppingCart.html");
+}
+
+
+const keepInCart = e => {
+
+    axios({
+        method: 'post',
+        url: 'https://goods-204a7.firebaseio.com/goods/getShoppingData',
+        data: {goodsValue},
+        success: function () {
+            Toastify({
+                text: "장바구니에 추가되었습니다.",
+                duration: 3000,
+                destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                positionLeft: false, // `true` or `false`
+                backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+            }).showToast();
+        },
+        error: function () {
+            Toastify({
+                text: "실패하였습니다.",
+                duration: 3000,
+                destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                positionLeft: false, // `true` or `false`
+                backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+            }).showToast();
+
+        }
+    })
+
+};
+           
+    
+
+
+
--- src/lecture05/shoppingCart/shoppingCart.js
+++ src/lecture05/shoppingCart/shoppingCart.js
@@ -1,3 +1,4 @@
 function openGoodsList() {
-    window.open("http://localhost:3006/src/lecture05/index.html");
-  }
+  window.open("http://localhost:3006/src/lecture05/index.html");
+}
+
