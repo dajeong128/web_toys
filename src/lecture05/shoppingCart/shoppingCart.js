12e5ff20ae5e3dc69611c9f63ef23206d8d26448
dajeong128 <dajeong128@naver.com>
2019-01-14 01:29:37 +0900

    add div element for  shopping cart list

--- src/lecture05/shoppingCart/shoppingCart.js
+++ src/lecture05/shoppingCart/shoppingCart.js
@@ -1,26 +1,46 @@
+
+document.body.onload = addElement;
+
+function addElement () { 
+  // create a new div element 
+  var newDiv = document.createElement("div"); 
+  // and give it some content 
+  var newContent = document.createTextNode("contents"); 
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);  
+
+  // add the newly created element and its content into the DOM 
+  var currentDiv = document.getElementById("shoppingCartList"); 
+  document.body.insertBefore(newDiv, currentDiv); 
+}
+
 const instance = axios.create({
   baseURL: 'https://goods-204a7.firebaseio.com/'
 })
 
+
+
+
 instance.get('cart.json')
-.then(res => {
-  const data = res.data
-  const contents = document.querySelector('goods-contents');
-  const list = document.querySelector('.shopping-cart-list')
-  const items = [];
+  .then(res => {
+    const data = res.data
+      console.log(data);
 
-  console.log(data);
-
-  items.push(`
+    const contents = document.querySelector('goods-contents');
+    // const list = document.querySelector('.shopping-cart-list')
+    const items = [];
+    items.push(`
     <li>
-        <p class="goods-name">상품명 : ${name}</p>
+        <p class="goods-name">상품명 : </p>
         <p class="goods-option">색깔 : / 사이즈 : </p>
         <p class="goods-price">가격 : </p>
         <span class="goods-shippingPrice"> +배송료 원</span>
     </li>
     `)
 
-    list.innerHTML = items.join('');
+    // list.innerHTML = items.join('');
+
+
   })
 
-
+  
