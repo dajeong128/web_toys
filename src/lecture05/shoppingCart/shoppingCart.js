e49c60aa55173e09b10366210d835d660b40ffc5
dajeong128 <dajeong128@naver.com>
2019-01-15 23:29:17 +0900

    creat shoppinggoods-contents whenever add cart list

--- src/lecture05/shoppingCart/shoppingCart.js
+++ src/lecture05/shoppingCart/shoppingCart.js
@@ -1,32 +1,23 @@
-
-document.body.onload = addElement;
-
-function addElement () { 
-  // create a new div element 
-  var newDiv = document.createElement("div"); 
-  // and give it some content 
-  var newContent = document.createTextNode("contents"); 
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);  
-
-  // add the newly created element and its content into the DOM 
-  var currentDiv = document.getElementById("shoppingCartList"); 
-  document.body.insertBefore(newDiv, currentDiv); 
-}
-
 const instance = axios.create({
   baseURL: 'https://goods-204a7.firebaseio.com/'
 })
 
-
-
-
 instance.get('cart.json')
   .then(res => {
-    const data = res.data
-      console.log(data);
+    const data = res.data;
+    const dataValue = Object.keys(data);
+    console.log(dataValue.length);
 
-    const contents = document.querySelector('goods-contents');
+      for (var i = 0; i<dataValue.length; i++) {
+        if (dataValue.length >  0) {
+        const addList = document.createElement("div")
+        const contents = document.querySelector('.goods-contents');
+        addList.className = 'shoppingCartList';
+
+        contents.appendChild(addList)        
+        }
+      }
+
     // const list = document.querySelector('.shopping-cart-list')
     const items = [];
     items.push(`
