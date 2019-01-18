ebef0d9ba548eeb8ccda966c0fc7db80c67cdd40
dajeong128 <dajeong128@naver.com>
2019-01-19 01:54:32 +0900

    add grand total

--- src/lecture05/shoppingCart/shoppingCart.js
+++ src/lecture05/shoppingCart/shoppingCart.js
@@ -11,19 +11,20 @@ instance.get('cart.json')
     // console.log(dataValue.length);
 
     /*post 데이터 수만큼 shoppingCartList div 추가 */
-    for (var i = 0; i < dataValue.length; i++) {
-      if (dataValue.length > 0) {
+    // for (var i = 0; i < dataValue.length; i++) {
+    //   if (dataValue.length > 0) {
         // const addList = document.createElement("div")
         // const contents = document.querySelector('.goods-contents');
         // addList.className = 'shoppingCartList';
         // const shoppingCartList = document.querySelector('.shoppingCartList')
-
         // contents.appendChild(addList)
 
         /* 장바구니데이터 노출 */
         var value = Object.values(data)
+        // console.log(value);
 
         var getdata = [];
+        var dataPrice = [];
 
         for (var i = 0; i < dataValue.length; i++) {
           if (dataValue.length > 0) {
@@ -34,14 +35,18 @@ instance.get('cart.json')
                 <p>상품명 : ${value[i].name}</p>
                 <p>색깔 : ${value[i].options.color} / 사이즈 : ${value[i].options.size} </p>
                 <p>가격 : ${value[i].price.toLocaleString()}원<br><span style = "color:grey; font-size: 13px;">+배송료 ${value[i].shipping.price.toLocaleString()}원</span></p>
-              </div>
+                </div>
               <div class="input">
                 <input class="quantity" type="number" min="1" max="100" value="1"> 
                 <i class="fas fa-trash-alt" onclick="removeItem(event)"></i>
               </div>
             </div>
            `)
-          }
+           var addTotal = dataPrice.push(value[i].price)
+           var reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+
+
           /*가져온데이터정보*/
           // console.log(getdata)
           
@@ -50,9 +55,25 @@ instance.get('cart.json')
           goodsContents.innerHTML = dataString;
         }
       }
-    }
-  })
+      /*금액누적*/
+      // console.log(dataPrice.reduce(reducer));
 
+      var total = document.querySelector('.total');
+      var price = [];
+      price.push (`
+        <div class="grandTotal">
+          <p>합계 ${dataPrice.reduce(reducer).toLocaleString()}원</p>
+        </div>
+      `)
+
+      var totalPrice = document.querySelector('.total');
+      totalPrice.innerHTML = price;
+
+  //   }
+  }
+  )
+
+ 
   const removeItem = (e)=> {
     Toastify({
       text: "장바구니에서 삭제되었습니다.",
@@ -64,8 +85,9 @@ instance.get('cart.json')
       positionLeft: false, // `true` or `false`
       backgroundColor: "linear-gradient(to right, #f03141, #fe7406)",
     }).showToast();
+
+    delete getdata
     e.target.parentElement.parentElement.remove();
-    // delete getdata
 
     // function deleteData(item, url) {
     //   return fetch(url + '/' + item, {
