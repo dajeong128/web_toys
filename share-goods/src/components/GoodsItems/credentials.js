723e727da7892b4fbd9f8d049d7fa47aaaa71bda
dajeong128 <dajeong128@naver.com>
2019-01-30 23:44:54 +0900

    refactor

--- share-goods/src/components/GoodsItems/credentials.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    databaseURL: "https://goods-204a7.firebaseio.com/goods.json",
-    credential: {
-      // JSON data from firebase-key.json
-      // service account key downloaded from firebase
-    }
-  };
