ea394cfeae08d2493144fd6291452f5c4886b1ba
uoon-dev <uoon.dev@gmail.com>
2019-02-02 10:51:13 +0900

    merge

--- share-goods/src/components/Header/Header.js
+++ share-goods/src/components/Header/Header.js
@@ -4,17 +4,17 @@ import './Header.css'
 
 
 const header = () => {
-    return (
-        <header>
-            <img src={logo} className="logo" />
-            <div class="navigation-items">
-                <ul>
-                    <li>상품목록</li>
-                    <li>장바구니</li>
-                </ul>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <img src={logo} className="logo" />
+      <div className="navigation-items">
+        <ul>
+          <li>상품목록</li>
+          <li>장바구니</li>
+        </ul>
+      </div>
+    </header>
+  )
 }
 
 export default header;
