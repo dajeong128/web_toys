e730592ae2a6c9c9d1fefbaf84cce35219c2fdef
dajeong128 <dajeong128@naver.com>
2019-02-02 10:37:36 +0900

    change class to className

--- share-goods/src/components/GoodsItems/firebase.js
+++ share-goods/src/components/GoodsItems/firebase.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import axios from 'axios'
 
 
@@ -12,37 +11,37 @@ class firebase extends React.Component {
   render() {
     const firebase = (props) => {
       instance.get('goods.json')
-        .then(res => {
-          const firebaseData = res.data
-          const items = []
-          return (
-            const options = []
-            const goodsItem = firebaseData[0].options
+      .then(res => {
+        const firebaseData = res.data
+        const items = []
+        const options = []
+        const goodsItem = firebaseData[0].options
 
-            options.push(`
-            <option value="${Object.keys(optionList).indexOf(id)}">
-              ${optionList.this.color} ${optionList.this.size} ${optionList.this.stock}
-            </option>
-            `)
+        return (
+          options.push(`
+          <option value="${Object.keys(optionList).indexOf(id)}">
+            ${optionList.this.color} ${optionList.this.size} ${optionList.this.stock}
+          </option>
+          `)
 
-            items.push(`
-              <li>
-              <p class="goods-name">${data[key].name}</p>
-              <select>
-              ${options.join('')};
-              </select>
-              <p class="goods-seller">${data[key].provider}</p>
-              <p class="goods-price">${data[key].price.toLocaleString()}원</p>
-              <span class="goods-shippingPrice"> +배송료 ${data[key].shipping.price.toLocaleString()}원</span>
-              <i class="fas fa-cart-plus" style="float:right; cursor:pointer;" onclick="keepInCart(event, ${Object.keys(data).indexOf(key)})"></i>
-              </li>
-            `)
-            items.join('');
-          }
+          items.push(`
+            <li>
+            <p className="goods-name">${data[key].name}</p>
+            <select>
+            ${options.join('')};
+            </select>
+            <p className="goods-seller">${data[key].provider}</p>
+            <p className="goods-price">${data[key].price.toLocaleString()}원</p>
+            <span className="goods-shippingPrice"> +배송료 ${data[key].shipping.price.toLocaleString()}원</span>
+            <i className="fas fa-cart-plus" style="float:right; cursor:pointer;" onclick="keepInCart(event, ${Object.keys(data).indexOf(key)})"></i>
+            </li>
+          `)
+          items.join('');
+        }
 
-        .catch(function (error) {
-          console.log(error);
-        })
+      .catch(function (error) {
+        console.log(error);
+      })
     }
   }
 }
